3107c6c510e0b4e7e047e1bebea2c9f898ed5a31
dubiao <dubiao@souche.com>
2018-03-13 11:03:15 +0800

    fix:修改依赖

--- sc-App/src/main/java/com/caicongyang/springcloudapp/main/Application.java
+++ sc-App/src/main/java/com/caicongyang/springcloudapp/main/Application.java
@@ -9,17 +9,17 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * Spring could web程序主入口
- * @author Administrator
  *
+ * @author Administrator
  */
 @Configuration//配置控制  
 @EnableAutoConfiguration//启用自动配置  
-@ComponentScan(value={"com.caicongyang.springcloudapp"})//组件扫描
+@ComponentScan(value = {"com.caicongyang.springcloudapp"})//组件扫描
 @EnableDiscoveryClient
 @EnableEurekaClient
 public class Application {
-	public static void main(String[] args) {   
+    public static void main(String[] args) {
         //第一个简单的应用，   
-        SpringApplication.run(Application.class,args);   
-    }   
+        SpringApplication.run(Application.class, args);
+    }
 }
